3925abfb2dd32c2fb65a9873e73241f6b7d0a68b
nevitatriya <triyanevita7@gmail.com>
2025-09-30 20:15:33 +0700

    Praktikum 4_soal no6

--- P5/function.php
+++ P5/function.php
@@ -1,6 +1,6 @@
 <?php
 
-function perkenalan($nama, $salam) {
+function perkenalan($nama, $salam="Assalamualaikum") {
     echo $salam.", ";
     echo "Perkenalakan, nama saya".$nama."<br/>";
     echo "Senang berkenalan dengan Anda<br/>";
@@ -13,5 +13,5 @@ echo"<hr>";
 $saya = " Triya";
 $ucapansalam = "Selamat pagi";
 
-perkenalan($saya, $ucapansalam);
+perkenalan($saya);
 ?>
