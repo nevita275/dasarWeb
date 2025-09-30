52ac94780d4f72225d61d5fc69e909d5a889d9e8
nevitatriya <triyanevita7@gmail.com>
2025-09-30 20:08:01 +0700

    Praktikum 4_soal no4

---
+++ P5/function.php
@@ -0,0 +1,12 @@
+<?php
+
+function perkenalan() {
+    echo "Assalamualaikum, ";
+    echo "Perkenalakan, nama saya Triya<br/>";
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+
+perkenalan();
+echo"<br>";
+perkenalan();
+?>
