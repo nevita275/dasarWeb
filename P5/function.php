fdb911186ff4adc5ee17ca8b5966cd66892209b4
nevitatriya <triyanevita7@gmail.com>
2025-09-30 20:31:00 +0700

    Praktikum 4_soal no8

--- P5/function.php
+++ P5/function.php
@@ -4,6 +4,13 @@ function hitungUmur($thn_lahir, $thn_sekarang) {
     $umur = $thn_sekarang - $thn_lahir;
     return $umur;
 }
+function perkenalan($nama, $salam="Assalamualaikum") {
+    echo $salam.", ";
+    echo "Perkenalakan, nama saya".$nama."<br/>";
+   
+    echo "Umur saya adalah ". hitungUmur(1988, 2023) ." tahun<br/>";
+    echo "Senang berkenalan dengan Anda<br/>";
+}
 
-echo "Umur saya adalah ". hitungUmur(1988, 2023) ." tahun";
+perkenalan(" Triya");
 ?>
