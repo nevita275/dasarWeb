fee8e01445dd2e94bbeba7fbf668b6ea2a1d9821
nevitatriya <triyanevita7@gmail.com>
2025-10-31 22:54:12 +0700

    Praktikum 7_soal 7.3

--- P7/form_validasi.php
+++ P7/form_validasi.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Form Input dengan Validasi</title>
+    <title>Form Input dengan Validasi & AJAX</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
 
-<h1>Form Input dengan Validasi</h1>
+<h1>Form Input dengan Validasi (AJAX)</h1>
 
-<form id="myForm" method="post" action="proses_validasi.php">
+<form id="myForm">
     <label for="nama">Nama:</label>
     <input type="text" id="nama" name="nama">
     <span id="nama-error" style="color: red;"></span><br>
@@ -17,12 +17,16 @@
     <input type="text" id="email" name="email">
     <span id="email-error" style="color: red;"></span><br>
 
-    <input type="submit" value="Submit">
+    <input type="submit" value="Kirim">
 </form>
+
+<div id="response" style="margin-top:10px; color:green;"></div>
 
 <script>
 $(document).ready(function() {
     $("#myForm").submit(function(event) {
+        event.preventDefault(); 
+
         var nama = $("#nama").val();
         var email = $("#email").val();
         var valid = true;
@@ -40,9 +44,20 @@ $(document).ready(function() {
         } else {
             $("#email-error").text("");
         }
-        if (!valid) {
-            event.preventDefault();
-            // Menghentikan pengiriman form jika validasi gagal
+
+        // Jika validasi lolos, kirim data via AJAX
+        if (valid) {
+            $.ajax({
+                url: "proses_validasi.php",
+                type: "POST",
+                data: { nama: nama, email: email },
+                success: function(response) {
+                    $("#response").html("Data berhasil dikirim!<br>Respon dari server: " + response);
+                },
+                error: function() {
+                    $("#response").html("<span style='color:red'>Terjadi kesalahan saat mengirim data.</span>");
+                }
+            });
         }
     });
 });
